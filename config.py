f53653bc5a99a02ff9489f1179e42b052be77f83
Harsha <harshagumpathi1331@gmail.com>
2026-04-25 15:39:01 +0530

    firstapp

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ class Config:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "fallback-secret")
 
     DB_CONFIG = {
-        "host": os.getenv("DB_HOST", "localhost"),
+        "host": os.getenv("DB_HOST", "mysql.railway.internal"),
         "user": os.getenv("DB_USER", "root"),
-        "password": os.getenv("DB_PASSWORD", ""),
-        "database": os.getenv("DB_NAME", "mydb"),
+        "password": os.getenv("DB_PASSWORD", "yqsGfPBhkbFmFDiGzIdQoGQteYkAGktR"),
+        "database": os.getenv("DB_NAME", "railway"),
         "port": int(os.getenv("DB_PORT", 3306))
     }
