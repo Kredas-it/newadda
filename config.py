eb811c4a530f33b1bb6f08c7d52ca8d7cf08bb22
Harsha <harshagumpathi1331@gmail.com>
2026-04-25 15:22:08 +0530

    firstapp

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 
 class Config:
-    JWT_SECRET_KEY = "super-secret-key"
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "fallback-secret")
+
     DB_CONFIG = {
-        "host": "localhost",
-        "user": "root",
-        "password": "Harsha@2004",
-        "database": "mydb"
+        "host": os.getenv("DB_HOST", "localhost"),
+        "user": os.getenv("DB_USER", "root"),
+        "password": os.getenv("DB_PASSWORD", ""),
+        "database": os.getenv("DB_NAME", "mydb"),
+        "port": int(os.getenv("DB_PORT", 3306))
     }
